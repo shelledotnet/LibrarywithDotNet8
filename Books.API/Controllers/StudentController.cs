9cb2ea43fcb25568222919904d19b82d939900f0
shelle mohammed olatunji <shelledotnet@outlook.com>
2025-01-26 05:15:37 +0100

    studentcontroller with a get endpoint

---
+++ Books.API/Controllers/StudentController.cs
@@ -0,0 +1,64 @@
+﻿using Books.API.Filter;
+using Books.domain.Models;
+using Books.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Books.API.Controllers
+{
+    [Produces("application/json", "application/xml")]  //output formatter Media type: Accept header
+    [Consumes("application/json")] //input-formatter Media type: content-type header
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(ServiceFailedResponse))]
+    [ServiceFilter(typeof(RequestAuthActionFilterAttribute))]
+    [TypeFilter(typeof(ApiKeyAuthorizationFilterAttribute))]//basicautorization for API
+    public class StudentController : ControllerBase
+    {
+
+        #region InMemmory-DataSource
+        private readonly List<Student> _students =
+        [
+            new Student
+            {
+                Id = 1,
+                Age=23,
+                Name="Adeola"
+               
+            },
+            new Student
+            {
+                Id = 7,
+                Age=12,
+                Name="Mariam"
+               
+            }
+        ];
+        #endregion
+
+
+
+
+
+
+        /// <summary>
+        ///get patient records
+        /// </summary>ServiceFailedResponse
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ServiceResponse<IEnumerable<Student>>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ServiceBadResponse))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ServiceFailedResponse))]
+        public IActionResult GetStudentName()
+        {
+            return  Ok( new ServiceResponse<IEnumerable<Student>>
+            {
+                Code = System.Net.HttpStatusCode.OK,
+                IsSuccess = true,
+                Message = "successful",
+                Data= _students
+
+            });
+        }
+    }
+}
---
+++ Books.Domain/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Domain.Models
+{
+  public  class Student
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public int Age { get; set; }
+
+
+    }
+}
